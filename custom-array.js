38100350534654c8681b537884cd5ed41b6a697d
th3fr3dy <taboutsafredy@gmail.com>
2023-11-04 00:45:29 +0100

    Add of pop() method customArray prototype ✌

--- custom-array.js
+++ custom-array.js
@@ -12,7 +12,15 @@ class customArray {
 
         return this.custArr
     }
+
+    // Supprime le dernier élément d'un tableau et renvoie cet élément.
+    pop () {
+        let lastValue = this.custArr[this.custArr.length - 1]
+        this.custArr.length = this.custArr.length - 1
+        return lastValue
+    }
 }
 
 let _customArray = new customArray([404, 505])
 _customArray.push("H2O", "28-01-2006")
+_customArray.pop()
